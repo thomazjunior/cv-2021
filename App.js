f0395d91585593c8eb724a6a92c167b49979ceb2
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-07 20:10:33 -0300

    Ambiente React Native configurado.

--- App.js
+++ App.js
@@ -2,20 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import MainStack from './src/MainStack';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack />
+      <StatusBar style="auto"/>
+    </NavigationContainer>
+    
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
